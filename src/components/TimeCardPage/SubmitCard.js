9983d3dcb031f3d26c2f5c6ad0b3b75f2565ddaf
hams7504 <malli.hamsini@gmail.com>
2023-04-06 19:40:29 -0400

    finished submitted modal ticket

--- src/components/TimeCardPage/SubmitCard.js
+++ src/components/TimeCardPage/SubmitCard.js
@@ -6,15 +6,27 @@ import Button from 'react-bootstrap/Button';
 
 
 
+
 export default function SubmitCard(props) {
 
     const [submitted, setSubmitted] = useState(false); 
     const [submitDate, setSubmitDate] = useState(null); 
+    const [state, setState] = useState('Rejected'); 
+    const [rejected, setRejected] = useState(true);
+    const [report_type, setReport] = useState('this is rejected'); 
+
+    const model = {
+        AuthorUUID: "XXXX",
+        Type: "Report / Comment, etc",
+        Timestamp: "",
+        Content: "invalid hours"
+    }
+
 
     useEffect(() => {
         //TODO - API Call to determine if the table has been submitted or not.
         //Will set submitted? here and also submitDate if it was submitted to grab the date 
-        
+        setReport(model.Content)        
     },[])
 
     const submitAction = () => {
@@ -23,10 +35,14 @@ export default function SubmitCard(props) {
         setSubmitDate(currentTime.toString()); 
     }
 
+
+
+    //setReport(model.Content)
+
     return (
         <div class="col-md-2" style={{ display: "flex", justifyContent: 'flex-end'}}>
             <Card 
-                bg={submitted ? 'success' : 'secondary'}
+                bg={(state === 'Completed') ? 'success' : ((state === 'In-Review: Employer'|| state === 'In-Review: Breaktime') ? 'warning' : 'danger') }
                 text="white"
                 key="submit_description"
                 className="mb-2 text-center"
@@ -35,11 +51,16 @@ export default function SubmitCard(props) {
                 <Card.Body>
                     <Button variant={submitted?'light':'light'} onClick={submitAction}>{submitted? "Resubmit" : "Submit!"}</Button>
                 </Card.Body>
+                {(submitted && rejected) && <Card.Footer text="white">
+                    {report_type}
+                        
+                    </Card.Footer>}
                 {submitted && <Card.Footer text="white">
                     {submitDate}
                         
                     </Card.Footer>}
             </Card>  
+
         </div>
     )
 
