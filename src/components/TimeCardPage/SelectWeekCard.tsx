e2659cffe36e55a8d81afc44589fc724b8dfb73b
aditya161216 <59684507+aditya161216@users.noreply.github.com>
2023-05-13 22:34:47 +0530

    clean up after rebase

--- src/components/TimeCardPage/SelectWeekCard.tsx
+++ src/components/TimeCardPage/SelectWeekCard.tsx
@@ -1,9 +1,5 @@
 import React, { useState, useEffect } from 'react'
-<<<<<<< HEAD
 import moment from 'moment';
-=======
-import { DateRangePicker } from 'react-date-range';
->>>>>>> Redid submit modal
 
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
@@ -13,49 +9,25 @@ import "react-datepicker/dist/react-datepicker.css";
 export default function DateCard(props) {
     const [date, setDate] = useState(undefined);
 
-<<<<<<< HEAD
     useEffect(() => {
-        const providedDate = props.date; 
+        const providedDate = props.date;
         if (providedDate === undefined) {
-            setDate(new Date()); 
+            setDate(new Date());
         } else {
             setDate(providedDate.toDate());
         }
-    },[])
+    }, [])
 
     useEffect(() => {
         if (date !== undefined) {
-            const startOfWeek = moment(date).startOf('week').unix(); 
-            props.onDateChange(moment.unix(startOfWeek)); 
+            const startOfWeek = moment(date).startOf('week').unix();
+            props.onDateChange(moment.unix(startOfWeek));
         }
     }, [date])
 
     return (
         <DatePicker selected={date} onChange={(date) => setDate(date)} />
-      );
-    }
-=======
-    const [selection, setSelection] = useState({
-        startDate: props.startDate,
-        endDate: props.endDate,
-        key: 'selection',
-        showDateDisplay: false
-    });
-
-    const selectDateRange = (interval) => {
-        const selectedRange = interval.selection;
-        props.onDateChange(selectedRange.startDate, selectedRange.endDate);
-
-        setSelection(selectedRange);
-    }
-
-    return (<div >
-        <DateRangePicker
-            ranges={[selection]}
-            onChange={selectDateRange}
-        />
-    </div>)
+    );
 }
->>>>>>> Redid submit modal
 
 
