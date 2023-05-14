c66c7ff252ab6c9f83a7c2e4f129ec6a2f30c0d7
Kaylee Wu <kaycolo8@gmail.com>
2023-05-14 17:09:58 -0400

    some deletions

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,8 +1,4 @@
 import React from 'react';
-// import Container from 'react-bootstrap/Container';
-// import Nav from 'react-bootstrap/Nav';
-// import Navbar from 'react-bootstrap/Navbar';
-// import NavDropdown from 'react-bootstrap/NavDropdown';
 import { PAGE_ROUTES, defaultColors } from 'src/constants';
 import {
   Box,
@@ -14,7 +10,6 @@ import {
   MenuList,
   MenuItem,
   Image,
-  Container,
   ButtonGroup
 } from '@chakra-ui/react'
 import {
