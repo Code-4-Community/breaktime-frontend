6af09b52e5b4a32451156641c2e3d4f095929be8
david <dinosaurlearning@gmail.com>
2023-06-21 17:49:36 -0400

    ran prettier

--- src/components/TimeCardPage/CellTypes/CommentCell.tsx
+++ src/components/TimeCardPage/CellTypes/CommentCell.tsx
@@ -1,177 +1,223 @@
-import React, {useState, useEffect, useContext} from 'react'; 
-import { UserContext } from '../UserContext';
+import React, { useState, useEffect, useContext } from "react";
+import { UserContext } from "../UserContext";
 import {
-    Button,
-    Modal,
-    ModalOverlay,
-    ModalContent,
-    ModalHeader,
-    ModalFooter,
-    ModalBody,
-    ModalCloseButton,
-    Editable,
-    EditableTextarea,
-    EditablePreview,
-    EditableInput,
-    Input,
-    IconButton,
-    ButtonGroup,
-    Flex,
-    useDisclosure,
-    useEditableControls,
-  } from '@chakra-ui/react';
+  Button,
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+  Editable,
+  EditableTextarea,
+  EditablePreview,
+  EditableInput,
+  Input,
+  IconButton,
+  ButtonGroup,
+  Flex,
+  useDisclosure,
+  useEditableControls,
+} from "@chakra-ui/react";
 import {
-    ChatIcon,
-    WarningIcon,
-    CheckIcon,
-    CloseIcon,
-    EditIcon
-  } from '@chakra-ui/icons';
+  ChatIcon,
+  WarningIcon,
+  CheckIcon,
+  CloseIcon,
+  EditIcon,
+} from "@chakra-ui/icons";
 
-
-import {CommentSchema} from '../../../schemas/RowSchema'; 
-import { UserSchema } from 'src/schemas/UserSchema';
-import {CommentType, CellStatus, Color} from '../types'; 
-import CommentModal from '../CommentModal'
-import moment from 'moment-timezone';
+import { CommentSchema } from "../../../schemas/RowSchema";
+import { UserSchema } from "src/schemas/UserSchema";
+import { CommentType, CellStatus, Color } from "../types";
+import CommentModal from "../CommentModal";
+import moment from "moment-timezone";
 
 interface CommentProps {
-    comments: CommentSchema[] | undefined; 
-    setComment: Function; // TODO: fix type 
-} 
-
+  comments: CommentSchema[] | undefined;
+  setComment: Function; // TODO: fix type
+}
 
 // setComments can be used for comments or reports
-interface ShowCommentModalProps{
-    comments: CommentSchema[];
-    setComments: Function;
-    icon; // TODO: add type
-    color: Color;
-    isEditable: boolean;
+interface ShowCommentModalProps {
+  comments: CommentSchema[];
+  setComments: Function;
+  icon; // TODO: add type
+  color: Color;
+  isEditable: boolean;
 }
 
-export const createNewComment = (user: UserSchema, type: CommentType, content: string) => {
-    return {
-      AuthorID: user.UserID, 
-      Type: type, 
-      Timestamp: moment().unix(), // TODO: possibly change it to be more specific formatting
-      Content: content, 
-      State: CellStatus.Active
-    }
-}
+export const createNewComment = (
+  user: UserSchema,
+  type: CommentType,
+  content: string
+) => {
+  return {
+    AuthorID: user.UserID,
+    Type: type,
+    Timestamp: moment().unix(), // TODO: possibly change it to be more specific formatting
+    Content: content,
+    State: CellStatus.Active,
+  };
+};
 
 function ShowCommentModal({
   comments,
   setComments,
   icon,
   color,
-  isEditable
+  isEditable,
 }: ShowCommentModalProps) {
-    const { isOpen, onOpen, onClose } = useDisclosure()
-    const [displayedComment, setDisplayedComment] = useState(comments[comments.length - 1]);
-    const user = useContext(UserContext);
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const [displayedComment, setDisplayedComment] = useState(
+    comments[comments.length - 1]
+  );
+  const user = useContext(UserContext);
 
-    const EditableControls = () => {
-      const {
-        isEditing,
-        getSubmitButtonProps,
-        getCancelButtonProps,
-        getEditButtonProps,
-      } = useEditableControls()
-      
-      // TODO: change this later to reflect figma
-      return isEditing ? (
-        <ButtonGroup justifyContent='center' size='sm'>
-          <Button leftIcon={<CheckIcon />} {...getSubmitButtonProps()} />
-          <Button leftIcon={<CloseIcon />} {...getCancelButtonProps()} />
-        </ButtonGroup>
-      ) : (
-        <Flex justifyContent='right'>
-          <Button size='sm' leftIcon={<EditIcon />} {...getEditButtonProps()} />
-        </Flex>
-      )
+  const EditableControls = () => {
+    const {
+      isEditing,
+      getSubmitButtonProps,
+      getCancelButtonProps,
+      getEditButtonProps,
+    } = useEditableControls();
+
+    // TODO: change this later to reflect figma
+    return isEditing ? (
+      <ButtonGroup justifyContent="center" size="sm">
+        <Button leftIcon={<CheckIcon />} {...getSubmitButtonProps()} />
+        <Button leftIcon={<CloseIcon />} {...getCancelButtonProps()} />
+      </ButtonGroup>
+    ) : (
+      <Flex justifyContent="right">
+        <Button size="sm" leftIcon={<EditIcon />} {...getEditButtonProps()} />
+      </Flex>
+    );
+  };
+
+  const elevatedUserPrivileges =
+    user.Type === "Supervisor" || user.Type === "Admin";
+
+  // probably need to add some more validation
+  const addNewComment = (value: string) => {
+    if (value !== "") {
+      setDisplayedComment(createNewComment(user, displayedComment.Type, value));
     }
+  };
 
-    const elevatedUserPrivileges = (user.Type === "Supervisor" || user.Type === "Admin")
+  const saveComment = () => {
+    setComments([...comments, displayedComment]);
+    // TODO: save to DB
+  };
 
-    // probably need to add some more validation
-    const addNewComment = (value: string) => {
-      if (value !== ""){
-        setDisplayedComment(createNewComment(user, displayedComment.Type, value))
-      }
-    }
+  // make the editable work as intended later, without the odd preview box and whatever
+  return (
+    <>
+      <IconButton
+        colorScheme={color}
+        aria-label="Report"
+        icon={icon}
+        onClick={onOpen}
+      />
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>{displayedComment.Type}</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            <Editable
+              isDisabled={!isEditable}
+              defaultValue={displayedComment.Content}
+              onSubmit={(value) => addNewComment(value)}
+            >
+              <Input as={EditableInput} />
+              <EditablePreview />
 
-    const saveComment = () => {
-      setComments([...comments, displayedComment])
-      // TODO: save to DB
-    }
-
-    // make the editable work as intended later, without the odd preview box and whatever
-    return (
-      <>
-        <IconButton colorScheme={color} aria-label='Report' icon={icon} onClick={onOpen} />
-        <Modal isOpen={isOpen} onClose={onClose}>
-          <ModalOverlay />
-          <ModalContent>
-            <ModalHeader>{displayedComment.Type}</ModalHeader>
-            <ModalCloseButton />
-            <ModalBody>
-                <Editable isDisabled={!isEditable} defaultValue={displayedComment.Content} onSubmit={(value) => addNewComment(value)}>
-                  <Input as={EditableInput} />
-                  <EditablePreview />
-                  {elevatedUserPrivileges && 
-                  <>
+              {elevatedUserPrivileges && (
+                <>
                   <EditableTextarea />
                   <EditableControls />
-                  </>}
-                </Editable> 
-            </ModalBody>
-  
-            <ModalFooter>
-              <Button colorScheme={Color.Blue} mr={3} onClick={onClose}>
-                Close
+                </>
+              )}
+            </Editable>
+          </ModalBody>
+
+          <ModalFooter>
+            <Button colorScheme={Color.Blue} mr={3} onClick={onClose}>
+              Close
+            </Button>
+            {isEditable && elevatedUserPrivileges && (
+              <Button colorScheme={Color.Green} onClick={saveComment}>
+                Save
               </Button>
-              {isEditable && elevatedUserPrivileges && <Button colorScheme={Color.Green} onClick={saveComment}>Save</Button>}
-            </ModalFooter>
-          </ModalContent>
-        </Modal>
-      </>
-    )
+            )}
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+    </>
+  );
 }
 
 const getAllCommentsOfType = (type, commentArray) => {
-    if (commentArray === undefined) {
-        return []
-    } else {
-        return commentArray.filter(comment => (comment.Type === type));
+  if (commentArray === undefined) {
+    return [];
+  } else {
+    return commentArray.filter((comment) => comment.Type === type);
+  }
+};
+
+export function CommentCell(props: CommentProps) {
+  const [comments, setComments] = useState(
+    getAllCommentsOfType(CommentType.Comment, props.comments)
+  );
+  const [reports, setReports] = useState(
+    getAllCommentsOfType(CommentType.Report, props.comments)
+  );
+  const [isEditable, setisEditable] = useState(false);
+  const user = useContext(UserContext);
+
+  useEffect(() => {
+    //Supervisor/Admins have the right to edit comments/reports
+    if (user.Type === "Supervisor" || user.Type === "Admin") {
+      setisEditable(true);
     }
-}
+  }, [user.Type]);
 
-export function CommentCell(props:CommentProps) {
-
-    const [comments, setComments] = useState(getAllCommentsOfType(CommentType.Comment, props.comments));
-    const [reports, setReports] = useState(getAllCommentsOfType(CommentType.Report, props.comments));
-    const [isEditable, setisEditable] = useState(false);
-    const user = useContext(UserContext);
-
-    useEffect(() => {
-        //Supervisor/Admins have the right to edit comments/reports
-        if (user.Type === "Supervisor" || user.Type === "Admin") {
-          setisEditable(true);
-        } 
-    }, [user.Type]);  
-
-    return (
-        <>
-            {reports.length > 0  ? 
-              <ShowCommentModal setComments={setReports} comments={reports} icon={<WarningIcon />} color={Color.Red} isEditable={isEditable} /> :
-              <CommentModal setComments={setReports} comments={reports} type={CommentType.Report} />}
-            {comments.length > 0 ? 
-              <ShowCommentModal setComments={setComments} comments={comments} icon={<ChatIcon />} color={Color.Blue} isEditable={isEditable} /> :
-              <CommentModal setComments={setComments} comments={comments} type={CommentType.Comment}/>}
-        </>
-    )
-
+  return (
+    <>
+      {reports.length > 0 ? (
+        <ShowCommentModal
+          setComments={setReports}
+          comments={reports}
+          icon={<WarningIcon />}
+          color={Color.Red}
+          isEditable={isEditable}
+        />
+      ) : (
+        <CommentModal
+          setComments={setReports}
+          comments={reports}
+          type={CommentType.Report}
+        />
+      )}
+      {comments.length > 0 ? (
+        <ShowCommentModal
+          setComments={setComments}
+          comments={comments}
+          icon={<ChatIcon />}
+          color={Color.Blue}
+          isEditable={isEditable}
+        />
+      ) : (
+        <CommentModal
+          setComments={setComments}
+          comments={comments}
+          type={CommentType.Comment}
+        />
+      )}
+    </>
+  );
 }
 
 // TODO: modify current comment modal to add for weekly or daily
--- src/components/TimeCardPage/CommentModal.tsx
+++ src/components/TimeCardPage/CommentModal.tsx
@@ -1,80 +1,90 @@
-//import { Menu, MenuButton, MenuItem } from '@aws-amplify/ui-react';
 import {
-    Input, Button, useDisclosure, Modal,
-    ModalContent, ModalHeader, VStack, StackDivider, ModalFooter, HStack, Center
-} from '@chakra-ui/react'
-import React, { useState, useContext } from 'react';
-import { Select } from '@chakra-ui/react'
-import { CommentType } from './types';
-import { CommentSchema } from 'src/schemas/RowSchema';
-import { createNewComment } from './CellTypes/CommentCell';
-import { UserContext } from './UserContext';
-// for now display samething for weekly and daily, if needed split into two components later
+  Input,
+  Button,
+  useDisclosure,
+  Modal,
+  ModalContent,
+  ModalHeader,
+  VStack,
+  StackDivider,
+  ModalFooter,
+  HStack,
+  Center,
+} from "@chakra-ui/react";
+import React, { useState, useContext } from "react";
+import { CommentType } from "./types";
+import { CommentSchema } from "src/schemas/RowSchema";
+import { createNewComment } from "./CellTypes/CommentCell";
+import { UserContext } from "./UserContext";
 
 // setComments can be used for comments or reports
-interface DailyCommentModalProps{
-    setComments:Function;
-    comments: CommentSchema[];
-    type:CommentType;
+interface DailyCommentModalProps {
+  setComments: Function;
+  comments: CommentSchema[];
+  type: CommentType;
 }
 
-export default function DailyCommentModal(props:DailyCommentModalProps) {
+export default function DailyCommentModal({
+  setComments,
+  comments,
+  type,
+}: DailyCommentModalProps) {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const [remark, setRemark] = useState();
+  const user = useContext(UserContext);
 
-    const { isOpen, onOpen, onClose } = useDisclosure()
-    const [remark, setRemark] = useState()
-    const user = useContext(UserContext);
+  // stores the remark that was made
+  const handleRemarkChange = (e) => {
+    setRemark(e.target.value);
+  };
 
-    // stores the remark that was made
-    const handleRemarkChange = (e) => {
-        setRemark(e.target.value)
-    }
+  const handleSubmit = () => {
+    setComments([...comments, createNewComment(user, type, remark)]);
+    // TODO: call to db
+  };
 
-    // duplicated code, can def be abstracted if needed
-    const handleSubmit = () => {
-        props.setComments([...props.comments, createNewComment(user, props.type, remark)])
-        console.log("saved")
-        // clean up abstractions and duplication otherwise fully done
-        // call to db
-    }
+  return (
+    <>
+      <Center>
+        <Button
+          gap={20}
+          display="flex"
+          onClick={onOpen}
+          bg="white"
+          color="black"
+        >
+          Comments/Reports
+        </Button>
+      </Center>
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalContent>
+          <VStack spacing={4} divider={<StackDivider />}>
+            <ModalHeader>New {type}</ModalHeader>
 
+            <form id="Form" onSubmit={handleSubmit}>
+              <HStack spacing={4}>
+                <label htmlFor="remarks">Remarks</label>
+                <Input
+                  id="remarks"
+                  name="remarks"
+                  type="text"
+                  onChange={handleRemarkChange}
+                  autoComplete="off"
+                />
+              </HStack>
+            </form>
 
-
-    return (
-        <>
-            <Center>
-                <Button gap={20} display="flex" onClick={onOpen} bg="white" color="black">Comments/Reports</Button>
-            </Center>
-            <Modal isOpen={isOpen} onClose={onClose}>
-                <ModalContent>
-                    <VStack spacing={4} divider={<StackDivider />}>
-                        <ModalHeader>Leave a comment/report</ModalHeader>
-
-                        <form id="Form" onSubmit={handleSubmit}>
-
-                            <HStack spacing={4}>
-                                <label htmlFor="remarks">Remarks</label>
-                                <Input
-                                    id="remarks"
-                                    name="remarks"
-                                    type="text"
-                                    onChange={handleRemarkChange}
-                                    autoComplete="off"
-                                />
-
-                            </HStack>
-
-                        </form>
-
-                        <ModalFooter>
-                            <HStack spacing={10}>
-                                <Button onClick={onClose}>Close</Button>
-                                <Button type='submit' onClick={handleSubmit}>Submit</Button>
-                            </HStack>
-                        </ModalFooter>
-
-                    </VStack>
-                </ModalContent>
-            </Modal>
-        </>
-    )
+            <ModalFooter>
+              <HStack spacing={10}>
+                <Button onClick={onClose}>Close</Button>
+                <Button type="submit" onClick={handleSubmit}>
+                  Submit
+                </Button>
+              </HStack>
+            </ModalFooter>
+          </VStack>
+        </ModalContent>
+      </Modal>
+    </>
+  );
 }
