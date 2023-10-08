a2da6f6f165a884e2aaca3ca71fca9d4821a1ea4
david <dinosaurlearning@gmail.com>
2023-10-08 13:15:02 -0700

    added underscore for timezone constant

--- src/constants.tsx
+++ src/constants.tsx
@@ -1,14 +1,6 @@
-import moment from "moment-timezone";
-import {
-  CellType,
-  CommentType,
-  Review_Stages,
-  CellStatus,
-} from "./components/TimeCardPage/types";
-
 export const TIMESHEET_DURATION = 7;
 
-export const TIMEZONE = "America/NewYork";
+export const TIMEZONE = "America/New_York";
 export const DATETIME_FORMAT = "MM/DD/YY";
 
 export const enum DEFAULT_COLORS {
